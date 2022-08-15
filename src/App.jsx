3b498e7356d866208711c2945369e920e4557103
Beatriz Camille <biacamille2019@gmail.com>
2022-08-15 19:53:41 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import {Header} from './components/Header';
+
+
+import styles from './App.module.css';
+import './global.css';
+import { Sidebar } from './components/Sidebar';
+
+function App() {
+ 
+
+  return (
+  
+  <div>
+     <Header />
+     <div className={styles.wrapper}>
+     <Sidebar/>
+      <main>
+        knshvdjgwwljjbjdgdkxjhvcdgydh
+      </main>
+     </div>
+  </div>
+
+   
+  )
+}
+
+export default App
